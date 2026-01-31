3b082d7bf78ecb6bbffb214802614b1445935041
wdm <wangdongming27@163.com>
2026-01-31 17:41:56 +0800

    添加项目文件。

---
+++ AspNetCore.Test/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace AspNetCore.Test
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            /*
+             * Simple Queue Productor
+
+            await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            const string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+            Console.WriteLine($" [x] Sent {message}");
+            */
+
+            await channel.QueueDeclareAsync(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            for (int i = 0; i < 5; i++)
+            {
+                var message = GetMessage(i);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = new BasicProperties
+                {
+                    Persistent = true,
+                };
+
+                await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "task_queue", mandatory: true, basicProperties: properties, body: body);
+                Console.WriteLine($" [x] Sent {message}");
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        static string GetMessage(int i)
+        {
+            // 根据索引生成包含不同数量点号的消息，模拟不同处理时间
+            int dots = i % 3 + 1; // 1-3个点
+            return "Hello World!" + i + new string('.', dots);
+        }
+    }
+}
---
+++ AspNetCore.Test2/Program.cs
@@ -0,0 +1,66 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Test2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            /* 
+             * Simple Queue Consumer
+             * 
+            await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine(" [*] Waiting for message.");
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+                return Task.CompletedTask;
+            };
+
+            await channel.BasicConsumeAsync("hello", autoAck: true, consumer: consumer);
+            */
+
+            await channel.QueueDeclareAsync(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received {message}");
+
+                int dots = message.Split('.').Length - 1;
+                await Task.Delay(dots * 1000);
+
+                Console.WriteLine(" [x] Done");
+
+                // here channel could also be accessed as ((AsyncEventingBasicConsumer)sender).Channel
+                await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+
+            await channel.BasicConsumeAsync("task_queue", autoAck: false, consumer: consumer);
+
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
